6af0f18a43dcfcc37dfec54f3c5bc0dfa5192684
gbburleigh <grahamburleigh2022@u.northwestern.edu>
2021-03-14 22:39:15 -0500

    added additional ff driver options to optimize memory and avoid memory leak issues causing invalidsessionid exception

--- newdriver.py
+++ newdriver.py
@@ -22,7 +22,13 @@ class Driver:
             options = Options()
             #options.add_argument('--headless')
             options.add_argument('--headless')
+            options.add_argument("start-maximized")
+            options.add_argument("disable-infobars")
+            options.add_argument("--disable-extensions")
+            #options.add_argument('--no-sandbox')
+            options.add_argument('--disable-application-cache')
             options.add_argument('--disable-gpu')
+            options.add_argument("--disable-dev-shm-usage")
             self.webdriver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), \
                 firefox_options=options)
         else:
